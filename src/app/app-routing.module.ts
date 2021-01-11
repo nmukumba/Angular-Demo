9f3a1e4fabb88220a8e7c9a9c8b59510274506d4
Norest Mukumba <norest@trust.net>
2021-01-11 16:22:03 +0200

    code refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo:'auth', pathMatch: 'full'},
+  { path: '', redirectTo: 'auth', pathMatch: 'full'},
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
 ];
