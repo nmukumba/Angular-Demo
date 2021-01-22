c830f87a1139cf5a561460afd8414f85bce6d51c
Norest Mukumba <norest@trust.net>
2021-01-22 16:58:17 +0200

    add more modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import { CookieService } from 'ngx-cookie-service';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,8 +19,9 @@ import {AppComponent} from './app.component';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     LeafletModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService],
   exports: [],
   bootstrap: [AppComponent]
 })
